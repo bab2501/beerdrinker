e35cbfe856e1ce90e42df3fae0e88dd8a1c2ae4f
Alexander Blaauwgeers <Alexander@Blaauwgeers.eu>
2018-12-13 13:40:38 +0100

    version 1.5

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install tmux python-pip git -y &> /dev/null
+pip install requests &> /dev/null
+mkdir gitdir/
+cd gitdir/
+git clone https://github.com/bab2501/beerdrinker.git
+cd beerdrinker/
+tmux new -t "beerdrinker"
