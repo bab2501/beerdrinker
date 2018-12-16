9b968791e43a37116e2bcba504c70b4f07f8e82f
Alexander Blaauwgeers <alexander@blaauwgeers.eu>
2018-12-16 13:58:52 +0100

    version 2.3

--- beerdrinker.py
+++ beerdrinker.py
@@ -70,13 +70,13 @@ def hostid():
 depositAccount(99, 1)
 desk = hostid()
 cafe = (hostid() + 1)
-depositAccount(desk, 10000)
-depositAccount(cafe, 10000)
+depositAccount(desk, 500)
+withdrawAccount(cafe, 500)
 
 # Drink beer ( pay 2 from me(5) to cafebank(4) )
 while True:
 	print(time.time())
-	for x in range(200):
+	for x in range(500):
 		transferAccount(desk, 1, cafe)
 	print(time.time())
 	time.sleep(15)
