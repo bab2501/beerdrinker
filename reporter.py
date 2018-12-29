0055ad8a61e170091118a0a0ad4e73f44bf56cca
Alexander Blaauwgeers <alexander@blaauwgeers.eu>
2018-12-29 22:58:39 +0100

    reporter.py for LS

---
+++ reporter.py
@@ -0,0 +1,43 @@
+#####################################
+###  BT metric v1
+###  a=bab2501 v= 1.0 d= 2018.12.29
+####################################
+
+###
+# Dependencies
+#########################
+import socket
+import time
+import os
+
+def hostid():
+	hostname = socket.gethostname()
+	return hostname
+	
+def hostname():
+	hostname = socket.gethostname()
+	hostid = int(hostname[-2:])
+	return hostid
+
+def tnow():
+	return int(time.time())
+
+def mnow():
+	return (tnow() %300 )
+
+print(tnow())
+print(mnow())
+print(hostname())
+
+while True:
+	if(mnow() < 4):
+		print(mnow)
+		os.system("start")
+		time.sleep(5)
+		print(mnow)
+		os.system("date >> report.log")
+		os.system("du -s /home/ablaauwgeers/.bitcoin >> report.log")
+		os.system("date >> report.log")
+	print(mnow())
+	time.sleep(1)
+time.sleep(1)o
